9d501594a41545104e0821f57059970cd202972c
Christian Westgaard <comlock@gmail.com>
2023-10-10 13:26:53 +0200

    Use string replace to convert commonjs2 to commonjs

--- bun/esm-to-cjs.node.js
+++ bun/esm-to-cjs.node.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { runTransform } = require("esm-to-cjs");
+const { match } = require('assert');
 // console.log('process.argv:%s', JSON.stringify(process.argv));
 
 const filePath = process.argv[2];
@@ -11,4 +12,16 @@ const fileContent = fs.readFileSync(filePath, 'utf-8');
 const result = runTransform(fileContent, filePath);
 // console.log('result:%s', result);
 
-fs.writeFileSync(filePath, result, 'utf-8');
+const regex = /module\.exports = {(?:\s*(\w+),?\s*)+}/s;
+
+const matches = result.match(regex);
+matches.shift();
+// console.log('matches:%s', JSON.stringify(matches, null, 4));
+
+const replaceMent = matches.map(match => `exports.${match} = ${match};`).join('\n');
+// console.log('replaceMent:%s', replaceMent);
+
+const replaced = result.replace(regex, replaceMent);
+// console.log('replaced:%s', replaced);
+
+fs.writeFileSync(filePath, replaced, 'utf-8');
